dfd5f6383a2aae66cb3672196a3e37acceca9cf0
MiracleWong <cfwr1991@126.com>
2019-03-10 11:52:42 +0800

    Ch5: 条件和循环

---
+++ src/ch5/condition/condition_test.go
@@ -0,0 +1,36 @@
+package operator_test
+
+import "testing"
+
+func TestWhileLoop(t *testing.T) {
+	n := 0
+	for n < 5 {
+		t.Log(n)
+		n++
+	}
+}
+func TestSwitchMultiCase(t *testing.T) {
+	for i := 0; i < 5; i++ {
+		switch i {
+		case 0, 2:
+			t.Log("Even")
+		case 1, 3:
+			t.Log("Odd")
+		default:
+			t.Log("it is not 0-3")
+		}
+	}
+}
+
+func TestSwitchCaseCondition(t *testing.T) {
+	for i := 0; i < 5; i++ {
+		switch { // no i in here
+		case i%2 == 0:
+			t.Log("Even")
+		case i%2 == 1:
+			t.Log("Odd")
+		default:
+			t.Log("it is not 0-3")
+		}
+	}
+}
