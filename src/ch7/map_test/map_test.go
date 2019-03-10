207cb917988321bb3514fa42404bee5a0fbca481
MiracleWong <cfwr1991@126.com>
2019-03-10 13:29:58 +0800

    Ch7：Map & 工厂模式

---
+++ src/ch7/map_test/map_test.go
@@ -0,0 +1,60 @@
+package operator_test
+
+import "testing"
+
+func TestInitMap(t *testing.T) {
+
+	m1 := map[int]int{1: 1, 2: 4, 3: 9}
+	t.Log(m1[2])
+	t.Logf("len of m1 is %d", len(m1))
+
+	m2 := map[int]int{}
+	m2[4] = 16
+	t.Log(m2[2])
+	t.Log(m2[4])
+	t.Logf("len of m2 is %d", len(m2))
+
+	m3 := make(map[int]int, 10)
+	t.Logf("len of m3 is %d", len(m3))
+}
+
+func TestAccessNotExistsKey(t *testing.T) {
+	m1 := map[int]int{}
+	t.Log(m1[1])
+
+	m1[2] = 0
+	t.Log(m1[2])
+	m1[3] = 0
+	if v, ok := m1[3]; ok {
+		t.Logf("key 3 value is %d", v)
+	} else {
+		t.Logf("key 3 value is not exists")
+	}
+
+}
+
+func TestTravelMap(t *testing.T) {
+	m1 := map[int]int{1: 1, 2: 4, 3: 9}
+	for k, v := range m1 {
+		t.Log(k, v)
+	}
+}
+
+func TestMapWithFunValue(t *testing.T) {
+	m := map[int]func(op int) int{}
+	m[1] = func(op int) int { return op }
+	m[2] = func(op int) int { return op * op }
+	m[3] = func(op int) int { return op * op * op }
+	// t.Log(m[1][2], m[2][2], m[3][2])
+}
+
+func TestMapForSet(t *testing.T) {
+	mySet := map[int]bool{}
+	mySet[1] = true
+	n := 1
+	if mySet[n] {
+		t.Logf("%d is existing", n)
+	} else {
+		t.Logf("%d is not existing", n)
+	}
+}
