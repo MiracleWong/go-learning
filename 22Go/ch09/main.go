7c18fcadbea1be0d21087c445454026b622eb73d
MiracleWong <cfwr1991@126.com>
2022-09-05 11:50:56 +0800

    增加 22Go 的 ch09 的 读写锁 sync.RWMutex

--- 22Go/ch09/main.go
+++ 22Go/ch09/main.go
@@ -10,7 +10,7 @@ import (
 
 var (
 	sum   int
-	mutex sync.Mutex
+	mutex sync.RWMutex
 )
 
 func main() {
@@ -18,14 +18,24 @@ func main() {
 	for i := 0; i < 100; i++ {
 		go add(10)
 	}
-
+	for i := 0; i < 10; i++ {
+		go fmt.Println("和为:", readSum())
+	}
 	// 防止提前退出
 	time.Sleep(2 * time.Second)
-	fmt.Println("和为:", sum)
+	//fmt.Println("和为:", sum)
 }
 
 func add(i int) {
 	mutex.Lock()
 	defer mutex.Unlock()
 	sum += i // 临界区
+}
+
+//增加了一个读取sum的函数，便于演示并发
+func readSum() int {
+	mutex.Lock()
+	defer mutex.Unlock()
+	b := sum
+	return b
 }
