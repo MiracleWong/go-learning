30038a4f71febe06b5023450975f496ef9332da3
MiracleWong <cfwr1991@126.com>
2022-09-09 09:20:56 +0800

    增加 22Go 的 ch10 的 context.WithCancel 的方式改造的 watchDog 函数

--- 22Go/ch10/main.go
+++ 22Go/ch10/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"sync"
 	"time"
@@ -9,22 +10,23 @@ import (
 func main() {
 	var wg sync.WaitGroup
 	wg.Add(1)
-	stopCh := make(chan bool)
+	ctx, stop := context.WithCancel(context.Background())
 	go func() {
 		defer wg.Done()
-		watchDog(stopCh, "[监控狗1]")
+		watchDog(ctx, "[监控狗1]")
 	}()
 	time.Sleep(5 * time.Second) //先让监控狗监控5秒
-	stopCh <- true              //发停止指令
+	stop()                      //发停止指令
 	wg.Wait()
 }
 
-func watchDog(stopCh chan bool, name string) {
+func watchDog(ctx context.Context, name string) {
 	// 开启for select循环，一直后台监控
 	for {
 		select {
-		case <-stopCh:
+		case <-ctx.Done():
 			fmt.Println(name, "停止指令已经收到，马上停止……")
+			return
 		default:
 			fmt.Println(name, "正在监控……")
 		}
