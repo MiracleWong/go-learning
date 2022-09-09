327fa928c56a6001c47ff38e1165487f9b46ee24
MiracleWong <cfwr1991@126.com>
2022-09-09 09:27:37 +0800

    增加 22Go 的 ch10 的 Context 传值

--- 22Go/ch10/main.go
+++ 22Go/ch10/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	var wg sync.WaitGroup
-	wg.Add(3)
+	wg.Add(4)
 	ctx, stop := context.WithCancel(context.Background())
 	go func() {
 		defer wg.Done()
@@ -23,9 +23,28 @@ func main() {
 		defer wg.Done()
 		watchDog(ctx, "[监控狗3]")
 	}()
+	valCtx := context.WithValue(ctx, "userId", 2)
+	go func() {
+		defer wg.Done()
+		getUser(valCtx)
+	}()
 	time.Sleep(5 * time.Second) //先让监控狗监控5秒
 	stop()                      //发停止指令
 	wg.Wait()
+}
+
+func getUser(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("获取用户", "协程退出")
+			return
+		default:
+			userId := ctx.Value("userId")
+			fmt.Println("获取用户", "用户ID为：", userId)
+			time.Sleep(1 * time.Second)
+		}
+	}
 }
 
 func watchDog(ctx context.Context, name string) {
